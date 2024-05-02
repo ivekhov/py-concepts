e377dd939f2c219fc943198703485320b9a92376
Ivan Vekhov <ivekhov@gmail.com>
2024-05-03 02:24:26 +0300

    add theory and ex-s on instances

--- oop_basics.py
+++ oop_basics.py
@@ -1,23 +1,31 @@
 class Counter:
-    value = 0
+    def __init__(self, value=0):
+        self.value = max(value, 0)
+        value = max(value, 0)
+
     def inc(self, delta=1):
-        self.value += delta
-    
+        return Counter(max((self.value + delta), 0))
+
     def dec(self, delta=1):
-        self.value -= delta
+        # return Counter(self.value - delta) # prev version
+        return self.inc(-delta) # cooler
+
+
+def test_counter():
+    c = Counter(-42)
+    assert c.value == 0
+    assert c.inc().inc(5).dec(2).value == 4
+    assert c.inc().inc().dec().value == 1
+    assert c.dec().dec().value == 0  # 0 is the minimum value
+    d = c.inc(100)
+    assert d.dec().value == 99
+    forty_two = Counter(42)
+    assert forty_two.value == 42
+
 
 
 def main():
-    c = Counter()
-    c.inc()
-    c.inc()
-    c.inc(40)
-    c.value  # 42
-    c.dec()
-    c.dec(30)
-    c.value  # 11
-    c.dec(delta=100)
-    c.value  # 0
+    test_counter()
 
 
 if __name__ == '__main__':
